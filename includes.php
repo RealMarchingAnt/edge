967dbbda90ea7ae2d85f66a33fd9319f662f561d
RealMarchingAnt <patrik.marchingant.johansson@gmail.com>
2015-08-17 22:31:08 +0200

    various small improvements

---
+++ includes.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patrikjohansson
+ * Date: 04/08/15
+ * Time: 19:35
+ */
+echo '
+    <link href="http://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="css/edge.css" rel="stylesheet">
+
+';
+
